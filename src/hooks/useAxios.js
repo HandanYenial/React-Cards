f071a87d41dfd61e881f0f755949c9e32249f945
Your Name <you@example.com>
2022-08-02 16:32:30 -0400

    updates

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,35 +2,36 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 
-function useAxios(keyInLS, baseUrl){
-    const[responses,setResponses] = useLocalStorage(keyInLS);
-
-    const addResponseData = async (formatter = data => data, restOfUrl = "") =>{
-        const response = await axios.get(`${baseUrl}${restOfUrl}`);
-        setResponses(data => [...data, formatter(response.data)]);
-    }
-
+function useAxios(keyInLS, baseUrl) {
+    const [responses, setResponses] = useLocalStorage(keyInLS);
+  
+    const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+      const response = await axios.get(`${baseUrl}${restOfUrl}`);
+      setResponses(data => [...data, formatter(response.data)]);
+    };
+  
     const clearResponses = () => setResponses([]);
-
-    return[responses, addResponseData, clearResponses];
-}
-
-function useLocalStorage(key, initialValue = []){
-    if(localStorage.getItem(key)){
-        initialValue = JSON.parse(localStorage.getItem(key));
+  
+    return [responses, addResponseData, clearResponses];
+  }
+  
+  function useLocalStorage(key, initialValue = []) {
+    if (localStorage.getItem(key)) {
+      initialValue = JSON.parse(localStorage.getItem(key));
     }
     const [value, setValue] = useState(initialValue);
-
+  
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
+      localStorage.setItem(key, JSON.stringify(value));
     }, [value, key]);
-
-    return[value,setValue];
-}
-
-export default useLocalStorage;
-
-export { useAxios , useLocalStorage };
+  
+    return [value, setValue];
+  }
+  
+  export default useLocalStorage;
+  
+  export { useAxios, useLocalStorage };
+  
 
 /**const useAxios = () => {
     const [data, setData] = useState([]);
